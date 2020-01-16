49899bb4d0b3c9050f48da5b0d74f95c5a9229c9
aluceps <aluceps@gmail.com>
2020-01-16 15:29:15 +0900

    support databinding

--- app/src/main/java/me/aluceps/skeleton/MainActivity.kt
+++ app/src/main/java/me/aluceps/skeleton/MainActivity.kt
@@ -1,12 +1,14 @@
 package me.aluceps.skeleton
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.aluceps.skeleton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
