bad4a3c8d97e630b54683fd79d177293d121e3bc
aluceps <aluceps@gmail.com>
2020-01-17 12:28:15 +0900

    fix to kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion("29.0.2")
+    defaultConfig {
+        applicationId = "me.aluceps.skeleton"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    dataBinding {
+        isEnabled = true
+    }
+}
+
+dependencies {
+    implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+
+    // Android Common Libraries
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    // Testing Libraries
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
